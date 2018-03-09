9b686855c3b899735317ee0fcc0abfc105703228
tomlim2 <younsoolim@cca.edu>
2018-03-09 10:19:01 -0800

    ew

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
   var scrlls1img= 0;
 
   $(window).scroll(function(){
@@ -57,3 +59,25 @@ $(document).ready(function(){
 
 
 });
+
+
+// Slideshow
+  var slideIndex = 1;
+
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+  showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("img-slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+  }
+// End Slideshow
