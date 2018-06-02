f83dde3711017753a0a66e3871319b4d91fd3845
tomlim2 <younsoolim@cca.edu>
2018-06-01 22:23:21 -0700

    jj

--- assets/js/jsver8.js
+++ assets/js/jsver8.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
-
-  var navtop= $(".pj-nav").offset().top;
-
-  $(window).scroll(function(){
-    var num = $(window).scrollTop();
-      if(num>navtop){
-        $(".pj-nav").addClass("fixed");
-      }else{
-        $(".pj-nav").removeClass("fixed");
-      }
-    });
+  // 
+  // var navtop= $(".pj-nav").offset().top;
+  //
+  // $(window).scroll(function(){
+  //   var num = $(window).scrollTop();
+  //     if(num>navtop){
+  //       $(".pj-nav").addClass("fixed");
+  //     }else{
+  //       $(".pj-nav").removeClass("fixed");
+  //     }
+  //   });
 
   $(window).scroll(function(){
     var num = $(window).scrollTop();
