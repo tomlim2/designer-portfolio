483ef1e246308b253354dbd3f8ef89b0f2a35097
tomlim2 <younsoolim@cca.edu>
2018-09-09 18:20:40 -0700

    eee

--- assets/js/three/bluecat.js
+++ assets/js/three/bluecat.js
@@ -1,9 +1,14 @@
 var container;
+
+var composerScene, composer;
+
 var camera, scene, renderer;
-var particleLight;
+var particleLight1, particleLight2, particleLight3;
 var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
+
+var effectVignette
 
 init();
 animate();
@@ -21,9 +26,28 @@ function init() {
   //
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
-  window.addEventListener('resize',onWindowResize,false);
+  window.addEventListener('resize', onWindowResize,false);
 
   fillScene();
+
+  //postprocessing
+  composer = new THREE.EffectComposer( renderer );
+  composer.addPass( new THREE.RenderPass( scene, camera ) );
+
+  shaderVignette = THREE.VignetteShader;
+  effectVignette = new THREE.ShaderPass( shaderVignette );
+
+  shaderSepia = THREE.SepiaShader;
+  effectSepia = new THREE.ShaderPass( shaderSepia );
+
+  effectSepia.uniforms[ "amount" ].value = 0.4;
+
+  effectVignette.uniforms[ "offset" ].value = 0.95;
+  effectVignette.uniforms[ "darkness" ].value = 1.2;
+
+  effectVignette.renderToScreen = true;
+  composer.addPass( effectSepia );
+  composer.addPass( effectVignette );
 
 }
 
@@ -35,11 +59,20 @@ function fillScene(){
   camera.position.z = 250;
 
   // scene
-  particleLight = new THREE.Object3D();
-  var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-  var prticle = new THREE.Mesh( new THREE.SphereBufferGeometry( 1, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
-  particleLight.add(pointLight, prticle);
-  scene.add( particleLight );
+  particleLight1 = new THREE.Object3D();
+  var pointLight = new THREE.PointLight( 0x14c0b2, 1.6, 50, 0.8  );
+  var prticle = new THREE.Mesh( new THREE.SphereBufferGeometry( 1, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0x14c0b2 } ) );
+  particleLight1.position.set(0,0,50);
+  particleLight1.add(pointLight, prticle);
+  scene.add( particleLight1 );
+  //
+  particleLight2 = new THREE.PointLight( 0x0040ff, 1.6, 50, 0.8 );
+  particleLight2.add( new THREE.Mesh( new THREE.SphereBufferGeometry( 1, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
+  scene.add( particleLight2 );
+
+  particleLight3 = new THREE.PointLight( 0xf7592a, 1.6, 50, 0.8 );
+  particleLight3.add( new THREE.Mesh( new THREE.SphereBufferGeometry( 1, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xf7592a } ) ) );
+  scene.add( particleLight3 );
 
   // camera.add( new THREE.AmbientLight( 0x222222 )  );
   scene.add( camera );
@@ -52,17 +85,17 @@ function fillScene(){
 
   var mtlLoader = new THREE.MTLLoader();
   mtlLoader.setPath( 'assets/js/three/3d/' );
-  mtlLoader.load( '3_lowpolyisland3_forweb.mtl', function( materials ) {
+  mtlLoader.load( '3_lowpolyisland4_forweb.mtl', function( materials ) {
 
     materials.preload();
 
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials( materials );
     objLoader.setPath( 'assets/js/three/3d/' );
-    objLoader.load( '3_lowpolyisland3_forweb.obj', function ( object ) {
-      object.position.set(40,-20,0);
+    objLoader.load( '3_lowpolyisland4_forweb.obj', function ( object ) {
+      object.position.set(60,-20,0);
       object.rotation.set(0,180*Math.PI/180,0);
-      object.scale.set(50,50,50);
+      object.scale.set(85,85,85);
       object1.add( object );
     });
   });
@@ -74,6 +107,7 @@ function onWindowResize(){
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight*0.94 );
+  composer.setSize( window.innerWidth, window.innerHeight*0.94 );
 }
 
 function onDocumentMouseMove( event ) {
@@ -96,10 +130,19 @@ function render() {
 
   camera.lookAt( scene.position );
 
-  var timer = Date.now() * 0.0001;
-  particleLight.position.x = Math.sin( timer * 7 ) * 80;
-  particleLight.position.y = Math.cos( timer * 5 ) * 160;
-  particleLight.position.z = Math.cos( timer * 3 ) * 80;
-  // object1.rotation.y += -0.005;
+  var timer = Date.now() * 0.00012;
+  particleLight1.position.x = Math.sin( timer * 7 ) * 80;
+  particleLight1.position.y = Math.cos( timer * 5 ) * 60;
+  particleLight1.position.z = Math.cos( timer * 3 ) * 80;
+
+  particleLight2.position.x = Math.cos( timer * 3 ) * 60;
+  particleLight2.position.y = Math.sin( timer * 5 ) * 80;
+  particleLight2.position.z = Math.sin( timer * 7 ) * 60;
+
+  particleLight3.position.x = Math.sin( timer * 7 ) * 60;
+  particleLight3.position.y = Math.cos( timer * 3 ) * 80;
+  particleLight3.position.z = Math.sin( timer * 5 ) * 60;
+
   renderer.render( scene, camera );
+  composer.render();
 }
