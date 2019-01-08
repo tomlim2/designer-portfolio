d05511698326fa64d6b50cc7dd851441fa06482d
tomlim2 <younsoolim@cca.edu>
2019-01-08 12:24:13 -0500

    about javascript

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -5,10 +5,8 @@ $(document).ready(function(){
   function randomNum(m,n) {
 		m = parseInt(m);
 		n = parseInt(n);
-
 		return Math.floor( Math.random() * (n - m + 1) ) + m;
 	};
-
 
   $(window).scroll(function(){
     var num = $(window).scrollTop();
@@ -31,5 +29,8 @@ $(document).ready(function(){
   $( "#logo" ).click(function() {
     $('.about-section').toggleClass('about-open');
   });
-
+  $( ".about" ).append(
+     "<p>Hi, I am Tom, a visual designer living in San Francisco.<br/>I make websites, 2D/3D motion, and branding.<br/><br/></p>",
+     "<p><a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >tomandlim@gmail.com</a><br/><a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>instagram</a></p>"
+   );
 });
