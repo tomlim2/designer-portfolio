edc387c84b02f863c72bb8e1fae19236f11d66fd
Younsoo Tom Lim <tomandlim@gmail.com>
2019-05-27 19:55:35 -0700

    ver10 3

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -29,10 +29,17 @@ $(document).ready(function(){
   $( ".home" ).append(home1);
   var menuAbout = "About Me"
   $( "#menu-about" ).append(menuAbout);
-  var about1 = "<p>Hi, I am Tom, a visual designer living in San Francisco, and have focused on experimenting mixture of graphic, animation, and programming, so my majority of graphic design work is screen-based media.<br/><br/>I make 2D/3D motion, and branding.<br/><br/></p>";
-  var about2 = "<p><a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >tomandlim@gmail.com</a><br/><a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>instagram</a></p>";
+  var intro = "<p>Hi, I am Tom, a visual designer living in San Francisco, and have focused on experimenting mixture of graphic, animation, and programming, so my majority of graphic design work is screen-based media. I make 2D/3D motion, and branding.<br/><br/>-</p>";
+  var contact = "<p><a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >tomandlim@gmail.com</a><br/><a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>instagram</a><br/><br/>-</p>";
+  var info1 = "<p><span class=\"empha\">Training</span><br/> BFA Graphic Design with high distinction - California College of the Arts, Spring 2019</p>";
+  var info2 = "<p><span class=\"empha\">Awards</span><br/> Student To Watch - Graphic Design USA, Spring 2019</p>";
+
   $( "#menu-about" ).click(function() {
     $('.about-section').toggleClass('about-open');
+    $( "#menu-about" ).toggleClass("selected");
   });
-  $( ".about" ).append(about1,about2);
+  $( ".about" ).append(
+    "<img src=\"assets/imgs/profile.jpg\"/>"
+  );
+  $( ".contact" ).append(intro,contact,info1,info2);
 });
