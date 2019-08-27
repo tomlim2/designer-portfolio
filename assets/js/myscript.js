610d5f5e01934172c1fdb166b4df9e383bb666d2
Younsoo Tom Lim <tomandlim@gmail.com>
2019-08-26 22:03:35 -0700

    qw

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
   var menuAbout = "ABOUT"
   $( "#menu-about" ).append(menuAbout);
   var intro = "<p>Hi, I am Tom, a visual designer living in San Francisco, and have focused on experimenting mixture of graphic, animation, and programming, so my majority of graphic design work is screen-based media. I make 2D/3D motion, and branding.<br/><br/>-</p>";
-  var contact = "<p><a href=\"https://linkedin.com/in/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>LinkedIn</a> | <a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>Instagram</a><br/><a href=\"https://drive.google.com/file/d/1RVDq7G53ELuPL2Dep6C645qouXTndu4Z/view?usp=sharing\" download>Resume</a> | <a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >E-mail</a><br/><br/>-</p>";
+  var contact = "<p><a href=\"https://linkedin.com/in/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>LinkedIn</a> | <a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>Instagram</a><br/><a href=\"https://drive.google.com/open?id=1XDcTl86O49hO0jwnfDbGLP6JKT7-Tmmi\" download>Resume</a> | <a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >E-mail</a><br/><br/>-</p>";
   var info1 = "<p><span class=\"empha\">Training</span><br/> BFA Graphic Design with high distinction - California College of the Arts, Spring 2019</p>";
   var info2 = "<p><span class=\"empha\">Awards</span><br/> Student To Watch - Graphic Design USA, Spring 2019</p>";
 
