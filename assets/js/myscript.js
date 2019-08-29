51a0301c450066a13137299564b7385d5af6ba36
Younsoo Tom Lim <tomandlim@gmail.com>
2019-08-29 02:12:00 -0700

    ver12

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -25,18 +25,18 @@ $(document).ready(function(){
         $(".headerNav").removeClass("fixed");
       }
   });
-  var home1 = "WORKS"
+  var home1 = "Works"
   $( ".home" ).append(home1);
-  var menuAbout = "ABOUT"
+  var menuAbout = "About"
   $( "#menu-about" ).append(menuAbout);
   var intro = "<p>Hi, I am Tom, a visual designer living in San Francisco, and have focused on experimenting mixture of graphic, animation, and programming, so my majority of graphic design work is screen-based media. I make 2D/3D motion, and branding.<br/><br/>-</p>";
-  var contact = "<p><a href=\"https://linkedin.com/in/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>LinkedIn</a> | <a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>Instagram</a><br/><a href=\"https://drive.google.com/open?id=1XDcTl86O49hO0jwnfDbGLP6JKT7-Tmmi\" download>Resume</a> | <a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >E-mail</a><br/><br/>-</p>";
-  var info1 = "<p><span class=\"empha\">Training</span><br/> BFA Graphic Design with high distinction - California College of the Arts, Spring 2019</p>";
-  var info2 = "<p><span class=\"empha\">Awards</span><br/> Student To Watch - Graphic Design USA, Spring 2019</p>";
+  var contact = "<p><a href=\"https://linkedin.com/in/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>LinkedIn</a> | <a href=\"https://www.instagram.com/tommlimm/\" target=\"_blank\" style='margin-top:1.5em'>Instagram</a><br/><a href=\"https://drive.google.com/open?id=1XDcTl86O49hO0jwnfDbGLP6JKT7-Tmmi\" download>Resume</a> | <a href=\"https://mail.google.com/mail/?view=cm&fs=1&to=tomandlim@gmail.com\" target=\"_blank\" >E-mail</a></p>";
+  var info1 = "<p><h2 class=\"empha\">Training</h2> BFA Graphic Design with high distinction - California College of the Arts, Spring 2019</p>";
+  var info2 = "<p><h2 class=\"empha\">Awards</h2> Student To Watch - Graphic Design USA, Spring 2019<br/><br/>-</p>";
 
   $( "#menu-about" ).click(function() {
     $('.about-section').toggleClass('about-open');
     $( "#menu-about" ).toggleClass("selected");
   });
-  $( ".contact" ).append(intro,contact,info1,info2);
+  $( ".contact" ).append(intro,info1,info2,contact,);
 });
